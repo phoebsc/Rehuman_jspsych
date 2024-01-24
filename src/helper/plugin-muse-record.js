17e17b0205f699795459b7dc7df5cf5e2e87ff03
Esteban Romero <73259884+esromerog@users.noreply.github.com>
2024-01-24 10:29:32 -0600

    Changes to Muse plugin and data uploading

--- src/helper/plugin-muse-record.js
+++ src/helper/plugin-muse-record.js
@@ -1,99 +1,195 @@
-import {initJsPsych} from 'jspsych';
-import { MuseClient } from 'muse-js';
+import { initJsPsych } from "jspsych";
+import { MuseClient } from "muse-js";
 
 const jsPsych = initJsPsych();
 
-
 const info = {
-    name: "html-muse-record",
-    parameters: {
-        /** The HTML string to be displayed */
-        stimulus: {
-            type: 'HTML_STRING',
-            default: undefined,
-        },
-        /** recording_id */
-        recording_id: {
-            type: 'STRING',
-            default: "",
-        },
-        /** How long to show the trial. */
-        recording_duration: {
-            type: 'INT',
-            default: 2000,
-        },
-        /** Label for the record again button */
-        record_again_button_label: {
-            type: 'STRING',
-            default: "Record again",
-        },
-        /** Label for the button to accept the audio recording (only used if allow_playback is true). */
-        accept_button_label: {
-            type: 'STRING',
-            default: "Continue",
-        },
+  name: "html-muse-record",
+  parameters: {
+    /** The HTML string to be displayed */
+    stimulus: {
+      type: "HTML_STRING",
+      default: undefined,
     },
+    instructions: {
+      type: "HTML_STRING",
+      default: "Connect your Muse 2 device",
+    },
+    /** recording_id */
+    recording_id: {
+      type: "STRING",
+      default: "",
+    },
+    /** How long to show the trial. */
+    recording_duration: {
+      type: "INT",
+      default: 2000,
+    },
+    /** Label for the record again button */
+    record_again_button_label: {
+      type: "STRING",
+      default: "Record again",
+    },
+    /** Label for the button to accept the audio recording (only used if allow_playback is true). */
+    accept_button_label: {
+      type: "STRING",
+      default: "Continue",
+    },
+    show_done_button: {
+      type: "BOOL",
+      default: true,
+    },
+    show_timer: {
+      type: "BOOL",
+      default: true,
+    },
+  },
 };
 //plug in class
 class MuseRecordPlugin {
   constructor(jsPsych) {
     this.jsPsych = jsPsych;
     this.muse = null;
+    this.isRecording = false;
   }
 
   trial(display_element, trial) {
     // display html elements
-    display_element.innerHTML = '<p>'+trial.stimulus+'</p>';
-    // connect, and then start
-    this.connectToMuse(display_element, trial).then(() => {
-      this.startRecording();
-    })
+    display_element.innerHTML = `
+    <div>${trial.instructions}</div>
+    <p><button class="jspsych-btn" id="connect-muse-btn">Connect</button></p>
+    `;
+    
+    let button = display_element.querySelector("#connect-muse-btn");
+    button.onclick = async () =>
+      await this.connectToMuse(display_element, trial);
 
-    // End recording after a set duration
-    this.jsPsych.pluginAPI.setTimeout(() => {
-      this.stopRecording();
-      this.endTrial(display_element, trial);
-    }, trial.recording_duration);
   }
 
-  connectToMuse(display_element, trial) {
+  async connectToMuse(display_element, trial) {
     // Create a new MuseDevice instance
+    console.log("Connect Press");
     this.muse = new MuseAPI(trial);
 
-    // Attempt to connect to the Muse device
-    return this.muse.connect()
-      .then(() => {
-        display_element.innerHTML += "<p>Connection status: "+this.muse.connected+'</p>';
-        // You can add more setup here if needed
-      })
-      .catch(error => {
-        display_element.innerHTML += "<p>Error: "+error+"</p>";
-        // Handle connection errors here
-      });
+    try {
+      await this.muse.connect();
+
+      display_element.innerHTML = `
+        <p>Connection successfull</p>
+        <p>${trial.stimulus}</p>
+        <button class="jspsych-btn" id="start-recording">Start</button>`;
+
+      let btn = display_element.querySelector("#start-recording");
+      btn.onclick = this.startExperiment;
+    } catch (error) {
+      let errorDiv = display_element.querySelector("#connection-error") || document.createElement('div');
+      errorDiv.textContent = `${error}`;
+      errorDiv.id = "connection-error";
+      display_element.appendChild(errorDiv);
+      console.log(error);
+    }
+  }
+
+  displayCountdown(durationInSeconds, clock) {
+    let countdownInterval = setInterval(function () {
+      // Calculate minutes and seconds
+      let minutes = Math.floor(durationInSeconds / 60);
+      let seconds = durationInSeconds % 60;
+
+      // Display the countdown in the HTML element
+      if (clock != undefined) {
+        clock.innerHTML = `${minutes}:${seconds}`;
+      }
+
+      // Decrease the remaining time
+      durationInSeconds--;
+
+      // Check if the countdown has reached zero
+      if (durationInSeconds < 0) {
+        clearInterval(countdownInterval);
+      }
+    }, 1000); // Update every second
+  }
+
+  startExperiment(display_element, trial) {
+    this.startRecording();
+
+    let html = `
+    <p>${trial.stimulus}</p>
+    `;
+
+    if (trial.show_timer) {
+      html += `<span id="clock"></span>`;
+    }
+
+    if (trial.show_done_button) {
+      html += `<button class=jspsych-btn id="finish-trial">${trial.done_button_label}</button>`;
+      const btn = display_element.querySelector("#finish-trial");
+      btn.onclick = () => {
+        this.endTrial(display_element);
+      };
+    }
+
+    display_element.innerHTML = html;
+
+    setTimeout(() => {
+      if (this.isRecording) {
+        this.endTrial(display_element, trial);
+      }
+    }, trial.recording_duration);
+    this.isRecording = true;
+    if (trial.show_timer) {
+      let clock = display_element.querySelector("#clock");
+      this.displayCountdown(trial.recording_duration, clock);
+    }
   }
 
   startRecording() {
     if (this.muse.connected) {
-      this.muse.stream()
+      this.muse.stream();
     }
   }
 
-  stopRecording() {
+  async stopRecording() {
+    this.isRecording = false;
     // Stop collecting data
-    this.muse.stop()
+    await this.muse.stop();
   }
 
-  endTrial(display_element, trial) {
-    // Process and return data
+  async endTrial(display_element, trial) {
+    await this.stopRecording();
+
+    var data = muse.recorded_data;
+    var muse_url = await this.uploadDataToURL(muse_data);
+
     var trial_data = {
-      // Data collected from MuseDevice
+      data,
+      muse_url,
     };
 
     // Clear display if needed
-    display_element.innerHTML = 'Recording is over.';
+    display_element.innerHTML = "Recording is over.";
 
     // Finish trial
     this.jsPsych.finishTrial(trial_data);
+  }
+
+  async uploadDataToURL(data) {
+    try {
+      const jsonBlob = new Blob([JSON.stringify(data, null, 2)], {
+        type: "application/json",
+      });
+      /*
+        const json = JSON.stringify(finalObject[key]);
+        const blob = new Blob([Papa.unparse(json)], {type: "text/csv"});
+      */
+      // Create a URL for the blob and trigger download
+      const url = URL.createObjectURL(jsonBlob);
+
+      return url;
+    } catch (error) {
+      console.error("Error uploading data", error);
+    }
   }
 }
 
@@ -106,18 +202,6 @@ class MuseAPI {
     3: "TP10",
   };
 
-  // Change if you want electrodes to have different weights in the power calculation
-  electrodePowerWeights = [1, 1, 1, 1];
-
-  // Band powers of EEG. The last index is written inclusively to use all powers less than the last index.
-  bandPowers = {
-    'Theta': [4, 8],
-    'Alpha': [8, 12],
-    "Low beta": [12, 16],
-    "High beta": [16, 25],
-    'Gamma': [25, 45],
-  };
-
   constructor(trial) {
     this.connected = false;
     this.dataArray = [];
@@ -126,33 +210,23 @@ class MuseAPI {
     this.muse = new MuseClient();
     this.id = this.muse.deviceName;
     this.connected = false;
-    this.id = trial.recording_id
+    this.id = trial.recording_id;
 
     this.numberOfChannels = 3;
-    const arrayLength = this.WINDOW_SIZE * this.sfreq;
 
-    this.buffer = new Array(this.numberOfChannels);
-    for (let i = 0; i <= this.numberOfChannels; i++) {
-      this.buffer[i] = new Array(arrayLength).fill(0);
-    }
-
-    for (const key in this.bandPowers) {
-      this.bandPowers[key] = [
-        this.bandPowers[key][0] * this.WINDOW_SIZE,
-        this.bandPowers[key][1] * this.WINDOW_SIZE - 1,
-      ];
-    }
     // Initialize stream and writer
     this.stream = new WritableStream({
       start(controller) {
         this.controller = controller;
       },
       cancel() {
-        console.log('Stream canceled');
-      }});
+        console.log("Stream canceled");
+      },
+    });
 
     // Create a writer for the stream
     this.writer = this.stream.getWriter();
+    this.recorded_data = {};
   }
 
   async connect() {
@@ -168,14 +242,18 @@ class MuseAPI {
       const info = await this.muse.deviceInfo();
       console.log(info);
       return info;
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      throw new Error("Unable to connect");
+    }
   }
 
-  async stream() {
+  stream() {
     this.muse.start();
     this.muse.eegReadings.subscribe((reading) => {
       if (this.connected) {
         this.dataArray.push(reading);
+        console.log(reading);
         if (this.dataArray.length == 4) {
           for (let i = 0; i < this.dataArray[0].samples.length; i++) {
             const dispatchDataArray = this.dataArray.map((data, index) => ({
@@ -183,102 +261,35 @@ class MuseAPI {
             }));
 
             console.log(dispatchDataArray);
-            const dispatchData = dispatchDataArray.reduce(
-              (acc, data) => {
-                acc[Object.keys(data)[0]] = data[Object.keys(data)[0]]
-                return acc
-              },
-              {}
-            );
+            const dispatchData = dispatchDataArray.reduce((acc, data) => {
+              acc[Object.keys(data)[0]] = data[Object.keys(data)[0]];
+              return acc;
+            }, {});
             console.log(dispatchData);
+
             // Instead of store.dispatch, write data to stream
             this.writer.write({
               id: this.id,
               data: dispatchData,
             });
           }
-
-          // Fill the data analysis buffer
-          this.dataArray.forEach((obj) => {
-            this.buffer[obj.electrode].splice(0, obj.samples.length);
-            this.buffer[obj.electrode].push(...obj.samples);
-          });
-
-          this.dataArray = [];
         }
       }
     });
     this.startMetricStream();
   }
-  _applyHanningWindow(signal) {
-    function hann(i, N) {
-      return 0.5 * (1 - Math.cos((6.283185307179586 * i) / (N - 1)));
-    }
-
-    const array = [];
-    for (let i = 0; i < signal.length; i++) {
-      array.push(signal[i] * hann(i, signal.length));
-    }
-    return array;
-  }
-  async _calculate_eeg_metrics() {
-    const res = Object.keys(this.bandPowers).reduce((acc, key) => {
-      acc[key] = 0;
-      return acc;
-    }, {});
-
-    for (let i = 0; i < this.numberOfChannels; i++) {
-      let sample = this.buffer[i];
-      sample = this._applyHanningWindow(sample); // Hanning window on the data
-
-      const fft = math.fft(sample);
-      let mag = fft.map((elem) =>
-        math.sqrt(elem.re * elem.re + elem.im * elem.im)
-      ); // Get the magnitude
-      mag = mag.splice(0, Math.floor(sample.length / 2)); // Get first bins of Fourier Transform
-      mag = mag.map((mag) => mag / sample.length); // Normalize FFT by sample length
-
-      for (const key in this.bandPowers) {
-        res[key] =
-          res[key] +
-          this.electrodePowerWeights[i] *
-            math.mean(
-              mag.slice(this.bandPowers[key][0], this.bandPowers[key][1])
-            );
-      }
-    }
-
-    for (const key in res) {
-      res[key] = res[key] / this.numberOfChannels;
-    }
-
-    // Instead of store.dispatch, write metric data to stream
-    this.writer.write({
-      id: this.id,
-      data: res,
-    });
-  }
-
-  async startMetricStream() {
-    this.metricStream = setInterval(() => {
-      if (this.connected) {
-        this._calculate_eeg_metrics();
-      } else {
-        clearInterval(this.metricStream);
-      }
-    }, 50);
-  }
 
   async stop() {
     try {
       // Stop the Muse device from sending data
-      await this.muse.stop();
+      this.muse.disconnect();
 
       // If you have a streaming mechanism, close the stream writer
       if (this.writer) {
         await this.writer.close();
       }
-      this.saveData();
+
+      this.recorded_data = await this.saveData();
 
       // Clear the metric stream interval if it exists
       if (this.metricStream) {
@@ -288,38 +299,27 @@ class MuseAPI {
       // Update the connected status
       this.connected = false;
 
-      console.log('Muse disconnected and streaming stopped');
+      console.log("Muse disconnected and streaming stopped");
     } catch (error) {
-      console.error('Error stopping Muse:', error);
+      console.error("Error stopping Muse:", error);
       // Handle any errors that occur while stopping
     }
   }
 
   // Method to save data from stream to a file
- async saveData() {
-  try {
-    // Close the writer to indicate we're done writing
-    await this.writer.close();
+  async saveData() {
+    try {
+      // Close the writer to indicate we're done writing
+      await this.writer.close();
 
-    // Convert the stream into a response and then into a JSON blob
-    const response = new Response(this.stream);
-    const data = await response.json(); // This assumes your data is in a format that can be directly converted to JSON
-    const jsonBlob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+      // Convert the stream into a response and then into a JSON blob
+      const response = new Response(this.stream);
+      const data = await response.json(); // This assumes your data is in a format that can be directly converted to JSON
 
-    // Create a URL for the blob and trigger download
-    const url = URL.createObjectURL(jsonBlob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'muse-data-'+this.id+'.json'; // Change the file extension to .json
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
-
-    console.log('Data saved as JSON');
-  } catch (error) {
-    console.error('Error saving data as JSON:', error);
-    // Handle any errors that occur during saving
-  }
+      return data;
+    } catch (error) {
+      console.error("Error posting data to URL:", error);
+    }
   }
 }
 
--- src/script.js
+++ src/script.js
@@ -1,13 +1,15 @@
 // // Import the functions you need from the SDKs you need
 // // https://firebase.google.com/docs/web/setup#available-libraries
 import { getDatabase, ref, set } from "firebase/database";
+import { getStorage, uploadBytes } from "firebase/storage";
 import { initializeApp } from "firebase/app";
-import {initJsPsych} from 'jspsych';
-import jsPsychHtmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
-import jsPsychSurveyText from '@jspsych/plugin-survey-text';
-import jsPsychInitializeMicrophone from '@jspsych/plugin-initialize-microphone';
-import jsPsychHtmlAudioResponse from '@jspsych/plugin-html-audio-response';
-import MuseRecordPlugin from './helper/plugin-muse-record';
+import { initJsPsych } from "jspsych";
+import jsPsychHtmlKeyboardResponse from "@jspsych/plugin-html-keyboard-response";
+import jsPsychSurveyText from "@jspsych/plugin-survey-text";
+import jsPsychInitializeMicrophone from "@jspsych/plugin-initialize-microphone";
+import jsPsychHtmlAudioResponse from "@jspsych/plugin-html-audio-response";
+import jsPsychInstructions from "@jspsych/plugin-instructions";
+import MuseRecordPlugin from "./helper/plugin-muse-record";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDnkXLLHTGtxFrCS-OByTWOOSds3KoOPVI",
@@ -16,114 +18,199 @@ const firebaseConfig = {
   storageBucket: "rehuman-test-a4bec.appspot.com",
   messagingSenderId: "531865232645",
   appId: "1:531865232645:web:f7535c3af2bf18e111436c",
-  databaseURL: "https://rehuman-test-a4bec-default-rtdb.firebaseio.com"
+  databaseURL: "https://rehuman-test-a4bec-default-rtdb.firebaseio.com",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const storage = getStorage();
 
 ///  length variables
-var length_recall = 3*60*1000;  // 3 minutes
-var length_muse_recording = 1*60*1000 // 1 min
+var length_recall = 3 * 60 * 1000; // 3 minutes
+var length_muse_recording = 1 * 60 * 1000; // 1 min
+length_muse_recording = 2000;
+
 /*
 helper functions to get the sona id
 */
 // function to extract data from URL variables
-function getUrlVars() { // #SONA
+function getUrlVars() {
+  // #SONA
   var vars = {}; // #SONA
-  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(m,key,value) { // #SONA
+  var parts = window.location.href.replace(
+    /[?&]+([^=&]+)=([^&]*)/gi,
+    function (m, key, value) {
+      // #SONA
       vars[key] = value; // #SONA
-  }); // #SONA
+    }
+  ); // #SONA
   return vars; // #SONA
 } // #SONA
 
 // function to go find data in the URL
 // should be provided a defualt (e.g., "") so that there will be no errors thrown
-function getUrlParam(parameter, defaultvalue){ // #SONA
+function getUrlParam(parameter, defaultvalue) {
+  // #SONA
   var urlparameter = defaultvalue; // #SONA
-  if(window.location.href.indexOf(parameter) > -1){ // #SONA
-      urlparameter = getUrlVars()[parameter]; // #SONA
-      } // #SONA
+  if (window.location.href.indexOf(parameter) > -1) {
+    // #SONA
+    urlparameter = getUrlVars()[parameter]; // #SONA
+  } // #SONA
   return urlparameter; // #SONA
+}
+
+function uploadDataToServer(data) {
+  const participantID = getUrlParam();
+  const audioRef = ref(storage, `audio/${participantID}`);
+  const eegRef = ref(storage, `eeg/${participantID}.csv`);
+
+  // Audio data
+  // data.trials.find(({trial-type})=>trial_type === "html-audio-response").response
+
+  /*
+  uploadBytes(audioRef, data.audio_file);
+  uploadBytes(eegRef, data.muse_recording);
+  */
+
+  fetch(data.audio_url)
+    .then((response) => response.blob())
+    .then((blob) => uploadBytes(audioRef, blob));
+
+  fetch(data.muse_url)
+    .then((response) => response.blob())
+    .then((blob) => uploadBytes(eegRef, blob));
+
+  set(ref(database, "participants/" + participantID), {
+    participantID,
+    info: {},
+  });
 }
 
 /*
 JSPSYCH code
 */
-var sona_id = getUrlParam('sona_id','Empty'); // #SONA
-var jsPsych = initJsPsych();
+
+var sona_id = getUrlParam("sona_id", "Empty"); // #SONA
+var jsPsych = initJsPsych({
+  on_finish: (data) => {
+    console.log("Finished");
+    console.log(data);
+  },
+});
 
 var survey_trial = {
   type: jsPsychSurveyText,
   questions: [
-  {prompt: "Name", name: 'name', value:" ", rows:1, columns:15, required:true},
-  {prompt: "Birth year (YYYY)", name: 'birth_year', value:" ", rows:1, columns:15, required:true}, 
-  {prompt: "Gender (F/M/other)", name: 'gender', value:"", rows:1, columns:15, required:true},
-  {prompt: "Handedness (RH/LH/both)", name: 'handedness', value:"", rows:1, columns:15, required:true},
+    {
+      prompt: "Name",
+      name: "name",
+      value: " ",
+      rows: 1,
+      columns: 15,
+      required: true,
+    },
+    {
+      prompt: "Birth year (YYYY)",
+      name: "birth_year",
+      value: " ",
+      rows: 1,
+      columns: 15,
+      required: true,
+    },
+    {
+      prompt: "Gender (F/M/other)",
+      name: "gender",
+      value: "",
+      rows: 1,
+      columns: 15,
+      required: true,
+    },
+    {
+      prompt: "Handedness (RH/LH/both)",
+      name: "handedness",
+      value: "",
+      rows: 1,
+      columns: 15,
+      required: true,
+    },
   ],
 };
 
-var allow_mic = {
-    type: jsPsychInitializeMicrophone
+var instruction_trial = {
+  type: jsPsychInstructions,
+  pages: [
+    "Welcome to the experiment. Click next to begin.",
+    "Be sure to have the Muse headband readily available for this experiment.",
+    "While we recording the data for this first stage, think about ...",
+  ],
+  button_label_next: "Continue",
+  button_label_previous: "Return",
+  show_clickable_nav: true,
 };
 
-var post_delay_ISI = function() {
-  return (Math.random()*0.2+1.2)*1000  //1.2-1.4
-}
+var allow_mic = {
+  type: jsPsychInitializeMicrophone,
+};
+
+var post_delay_ISI = function () {
+  return (Math.random() * 0.2 + 1.2) * 1000; //1.2-1.4
+};
 var post_delay_blank = {
-   type: jsPsychHtmlKeyboardResponse,
-   stimulus:'',
-   trial_duration: post_delay_ISI
-}
+  type: jsPsychHtmlKeyboardResponse,
+  stimulus: "",
+  trial_duration: post_delay_ISI,
+};
 var recording_block = {
   type: MuseRecordPlugin,
-  stimulus: 'close your eyes for one minute.',
-  recording_id: 'test',
-  recording_duration: length_muse_recording
-}
-var recall = {
-    type: jsPsychHtmlAudioResponse,
-    stimulus: 
-        '<p>Please sing the first few seconds of a song and click the button when you are done.</p>'
-        +'<div style="font-size: 50px; color: blue"><p><span id="clock">3:00</span></div></p>',
-    recording_duration: length_recall,
-    show_done_button: true,
-    done_button_label: 'I am done.',
-    on_load: function(){
-      window.clear_timer = 1;
-      var wait_time = length_recall-200; // in milliseconds
-      var start_time = performance.now();
-      // document.querySelector('button').disabled = true;
-      var interval = setInterval(function(){
-          var time_left = wait_time - (performance.now() - start_time);
-          var minutes = Math.floor(time_left / 1000 / 60);
-          var seconds = Math.floor((time_left - minutes*1000*60)/1000);
-          var seconds_str = seconds.toString().padStart(2,'0');
-          if(time_left <= 0){
-              document.querySelector('#clock').innerHTML = "0:00";
-              window.clear_timer = -1;
-          };
-          if(window.clear_timer > 0){
-              document.querySelector('#clock').innerHTML = minutes + ':' + seconds_str
-          }else {
-              clearInterval(interval);
-          };
-      }, 250)
-      },
-    on_finish: function(data){
-        window.clear_timer = -1;
-        // fetch('/save-my-data.php', { audio_base64: data.response })
-        //     .then((audio_id){
-        //         data.response = audio_id;
-        //     });
-    }
+  stimulus: "Close your eyes for one minute.",
+  instructions: "Connect your Muse headband using Bluetooth.",
+  recording_id: "test",
+  recording_duration: length_muse_recording,
 };
 
+var recall = {
+  type: jsPsychHtmlAudioResponse,
+  stimulus:
+    "<p>Please sing the first few seconds of a song and click the button when you are done.</p>" +
+    '<div style="font-size: 50px; color: blue"><p><span id="clock">3:00</span></div></p>',
+  recording_duration: length_recall,
+  show_done_button: true,
+  done_button_label: "I am done.",
+  on_load: function () {
+    window.clear_timer = 1;
+    var wait_time = length_recall - 200; // in milliseconds
+    var start_time = performance.now();
+    // document.querySelector('button').disabled = true;
+    var interval = setInterval(function () {
+      var time_left = wait_time - (performance.now() - start_time);
+      var minutes = Math.floor(time_left / 1000 / 60);
+      var seconds = Math.floor((time_left - minutes * 1000 * 60) / 1000);
+      var seconds_str = seconds.toString().padStart(2, "0");
+      if (time_left <= 0) {
+        document.querySelector("#clock").innerHTML = "0:00";
+        window.clear_timer = -1;
+      }
+      if (window.clear_timer > 0) {
+        document.querySelector("#clock").innerHTML =
+          minutes + ":" + seconds_str;
+      } else {
+        clearInterval(interval);
+      }
+    }, 250);
+  },
+  on_finish: function (data) {
+    window.clear_timer = -1;
+    // fetch('/save-my-data.php', { audio_base64: data.response })
+    //     .then((audio_id){
+    //         data.response = audio_id;
+    //     });
+  },
+};
 
 // define final timeline
-var timeline = []
+var timeline = [];
 // timeline.push(survey_trial)
 timeline.push(recording_block);
-timeline.push(allow_mic, recall)
-jsPsych.run(timeline)
+timeline.push(allow_mic, recall);
+jsPsych.run(timeline);
